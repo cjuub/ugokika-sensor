26e78e3c65c6bf454b25fddd6f3a2cd8564e85cf
cjuub <cjuuub@gmail.com>
2018-01-02 14:03:20 +0100

    Enable OTA updates.

--- ugokika-sensor.ino/ugokika-sensor.ino.ino
+++ ugokika-sensor.ino/ugokika-sensor.ino.ino
@@ -1,6 +1,9 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 
+#include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
+
 ESP8266WiFiMulti WiFiMulti;
 
 const char* WIFI_SSID = "ssid";
@@ -9,13 +12,12 @@ const char* WIFI_PASSWORD = "password";
 const char* UGOKIKA_SERVER_HOST = "192.168.1.10";
 const uint16_t UGOKIKA_SERVER_PORT = 13337;
 
-const int HEART_BEAT_INTERVAL_MS =  60000;
+const int HEART_BEAT_INTERVAL_MS = 60000;
 
 void setup() {
-    Serial.begin(115200);
-    delay(10);
-
     pinMode(13, INPUT);
+    
+    Serial.begin(115200);
 
     WiFiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
     
@@ -27,6 +29,38 @@ void setup() {
         delay(500);
     }
 
+    // Port defaults to 8266
+    // ArduinoOTA.setPort(8266);
+
+    // Hostname defaults to esp8266-[ChipID]
+    // ArduinoOTA.setHostname("myesp8266");
+  
+    // No authentication by default
+    // ArduinoOTA.setPassword((const char *)"123");
+  
+    ArduinoOTA.onStart([]() {
+      Serial.println("Start");
+    });
+    
+    ArduinoOTA.onEnd([]() {
+      Serial.println("\nEnd");
+    });
+    
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    
+    ArduinoOTA.onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+    ArduinoOTA.begin();
+
     Serial.println("");
     Serial.println("OK");
     Serial.println("Sensor IP address: ");
@@ -37,6 +71,7 @@ void setup() {
 
 
 void loop() {
+    ArduinoOTA.handle();
     WiFiClient client;
 
     Serial.print("Connecting to ");
